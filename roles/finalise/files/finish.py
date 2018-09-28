b13734a412f7c1acd1d5e7fb044b88c3da0bf0a3
Matt Williams <matt@milliams.com>
2018-09-28 15:17:30 +0100

    Add ansible run to finish script

--- roles/finalise/files/finish.py
+++ roles/finalise/files/finish.py
@@ -28,4 +28,5 @@ if not os.path.exists('/home/opc/users.yml'):
     print('It should contain the users you want to have access to the system.')
     exit(1)
 
-#subprocess.check_call([], stdout=subprocess.STDOUT, stderr=subprocess.STDOUT)
+subprocess.check_call(['ansible-playbook', '--inventory=/home/opc/hosts', '--extra-vars=/home/opc/users.yml', 'users.yml'],
+                      stdout=subprocess.STDOUT, stderr=subprocess.STDOUT, cwd='/home/opc/slurm-ansible-playbook')
