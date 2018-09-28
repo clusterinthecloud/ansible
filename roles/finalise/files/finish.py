0b162441450e24cca556c7d2c14b5d780a0a77af
Matt Williams <matt@milliams.com>
2018-09-28 14:00:02 +0100

    Move finalising to a role

---
+++ roles/finalise/files/finish.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python2
+
+from __future__ import (absolute_import, division, print_function)
+import glob
+import os
+
+finished_nodes = set(os.path.basename(file) for file in glob.glob('/mnt/shared/finalised/*'))
+
+all_nodes = set()
+with open('/home/opc/hosts') as all_node_config:
+    for line in all_node_config:
+        if not line.startswith('['):
+            all_nodes.add(line.strip())
+
+unfinished_nodes = all_nodes - finished_nodes
+
+if unfinished_nodes:
+    print('Error: The following nodes have not reported finishing their setup:')
+    for node in sorted(unfinished_nodes):
+        print(' ', node)
+    print('Please allow them to finish before continuing.')
+    print('For information about why they have not finished, SSH to that machine and check the file /home/opc/ansible-pull.log')
+    exit(1)
+
+print('Please create the user config file and rerun this script...')
+
+
