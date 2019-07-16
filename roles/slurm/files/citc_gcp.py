37f867c411cc5cd28757d1727e2661cf19fcb011
davidyoung <david.young@appsbroker.com>
2019-07-16 13:35:12 +0100

    Adding GCP creation

---
+++ roles/slurm/files/citc_gcp.py
@@ -0,0 +1,234 @@
+import base64
+import re
+import subprocess
+import time
+from typing import Dict, Optional, Tuple
+from google.oauth2 import service_account
+from google.auth.transport.requests import AuthorizedSession
+import json
+import googleapiclient.discovery
+import logging
+import yaml
+
+__all__ = ["get_nodespace", "start_node"]
+
+
+def load_yaml(filename: str) -> dict:
+    with open(filename, "r") as f:
+        return yaml.safe_load(f)
+
+
+def get_nodespace(file="/etc/citc/startnode.yaml") -> Dict[str, str]:
+    """
+    Get the information about the space into which we were creating nodes
+    This will be static for all nodes in this cluster
+    """
+    return load_yaml(file)
+
+
+def get_subnet(gce_compute, compartment_id: str) -> str:
+    """
+    Get the relevant cluster subnet for a given compartment, VCN and AD
+    """
+    return "global/networks/default"
+
+
+
+
+def get_node(gce_compute, log, compartment_id: str, zone: str, hostname: str) -> str:
+    filter_clause = f'(name={hostname})'
+    
+    result = gce_compute.instances().list(project=compartment_id,zone=zone, filter=filter_clause).execute()
+    log.info(f'get status {result}')
+    item = result['items'][0] if 'items' in result else None
+    log.info(f'get items {item} ')
+    return item
+
+
+def get_node_state(gce_compute, log, compartment_id: str, zone: str, hostname: str) -> str:
+    """
+    Get the current node state of the VM for the given hostname
+    If there is no such VM, return "TERMINATED"
+    """
+
+    item = get_node(gce_compute, log, compartment_id, zone, hostname)
+    
+    if item:
+        status = item['status']
+        return status
+    return None
+    #still_exist = [i for i in result if i['state'] != "TERMINATED"]
+    #if not still_exist:
+    #    return "TERMINATED"
+    #if len(still_exist) > 1:
+    #    log.error(f"Multiple matches found for {hostname}")
+    #return still_exist[0].lifecycle_state
+
+def get_ip_for_vm(gce_compute, log, compartment_id: str, zone: str, hostname: str) -> str:
+    
+    filter_clause = f'(name={hostname})'
+    item = get_node(gce_compute, log, compartment_id, zone, hostname)
+    
+    network = item['networkInterfaces'][0]
+    log.info(f'network {network}')
+    ip = network['networkIP']
+    log.info(f'ip address = {ip}')
+    return ip
+
+def get_shape(hostname):
+
+    #features = subprocess.run(["sinfo", "--Format=features:200", "--noheader", f"--nodes={hostname}"], stdout=subprocess.PIPE).stdout.decode().split(',')
+    #shape = [f for f in features if f.startswith("shape=")][0].split("=")[1].strip()
+    return('n1-standard-1')
+
+
+def create_node_config(gce_compute, hostname: str, ip: Optional[str], nodespace: Dict[str, str], ssh_keys: str):
+    """
+    Create the configuration needed to create ``hostname`` in ``nodespace`` with ``ssh_keys``
+    """
+    shape = get_shape(hostname)
+    subnet = get_subnet(gce_compute, nodespace["compartment_id"])
+    zone = nodespace["zone"]
+
+    #with open("/home/slurm/bootstrap.sh", "rb") as f:
+    #    user_data = base64.b64encode(f.read()).decode()
+
+    machine_type = f"zones/{zone}/machineTypes/{shape}"
+    
+    image_response = gce_compute.images().getFromFamily(
+        project='gce-uefi-images', family='centos-7').execute()
+    source_disk_image = image_response['selfLink']
+
+    config = {
+        'name': hostname,
+        'machineType': machine_type,
+
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': source_disk_image,
+                }
+            }
+        ],
+        'networkInterfaces': [
+            {
+                'network': subnet,
+                'addressType': 'INTERNAL',
+                'address': ip,            
+                'accessConfigs': [
+                    {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+                ]
+            }
+        ]
+    }
+
+    return config
+
+
+def get_ip(hostname: str) -> Tuple[Optional[str], Optional[str], Optional[str]]:
+    host_dns_match = re.match(r"(\d+\.){3}\d+", subprocess.run(["host", hostname], stdout=subprocess.PIPE).stdout.decode().split()[-1])
+    dns_ip = host_dns_match.group(0) if host_dns_match else None
+
+    slurm_dns_match = re.search(r"NodeAddr=((\d+\.){3}\d+)", subprocess.run(["scontrol", "show", "node", hostname], stdout=subprocess.PIPE).stdout.decode())
+    slurm_ip = slurm_dns_match.group(1) if slurm_dns_match else None
+
+    ip = dns_ip or slurm_ip
+
+    return ip, dns_ip, slurm_ip
+
+
+def get_build():
+    credentials = service_account.Credentials.from_service_account_file(
+            '/home/davidy/secrets/ex-eccoe-university-bristol-52b726c8a1f3.json')
+    compute = googleapiclient.discovery.build('compute', 'v1', 
+        credentials=credentials, cache_discovery=False)
+    
+
+    return(compute)
+
+def wait_for_operation(compute, log, project, zone, operation):
+    log.info('Waiting for operation to finish...')
+    while True:
+        result = compute.zoneOperations().get(
+            project=project,
+            zone=zone,
+            operation=operation).execute()
+        log.info("Wait result=%s", result)
+        if result['status'] == 'DONE':
+            logging.info("done.")
+            if 'error' in result:
+                raise Exception(result['error'])
+            return result
+
+        time.sleep(1)
+
+def start_node( log, host: str, nodespace: Dict[str, str], ssh_keys: str) -> None:
+    
+    project=nodespace["compartment_id"]
+    zone=nodespace["zone"]
+
+    log.info(f"Starting {host} in {project} {zone}")
+    
+    gce_compute = get_build()
+
+    while get_node_state(gce_compute, log, project, zone, host) == "TERMINATING":
+        log.info(" host is currently terminating. Waiting...")
+        time.sleep(5)
+
+    node_state = get_node_state(gce_compute, log, project, zone, host)
+    if node_state:
+        log.warning(f" host is already running with state {node_state}")
+        return
+
+    ip, _dns_ip, slurm_ip = get_ip(host)
+
+    instance_details = create_node_config(gce_compute, host, ip, nodespace, ssh_keys)
+
+
+    try:
+        response = gce_compute.instances().insert(
+            project=project,
+            zone=zone,
+            body=instance_details).execute()
+        wait_for_operation(gce_compute, log, project, zone, response['name'])
+        vm_ip = get_ip_for_vm(gce_compute, log, project, zone, host)
+    except Exception as e:
+        log.error(f" problem launching instance: {e}")
+        return
+
+    if not slurm_ip:        
+        private_ip = vm_ip
+
+        log.info(f"  Private IP {private_ip}")
+
+        ## Can't we just update this anyway?
+        subprocess.run(["scontrol", "update", f"NodeName={host}", f"NodeAddr={private_ip}"])
+
+    log.info(f" Started {host}")
+    return vm_ip
+
+
+# [START run]
+def do_create_instance():
+
+    logging.basicConfig(format='%(asctime)s %(message)s',level=logging.INFO)
+    log = logging.getLogger("startnode")    
+    
+    
+    #project = 'ex-eccoe-university-bristol'
+    #zone = 'europe-west4-a'
+    name = 'dy-test-node1'
+    
+    log.info('Creating instance.')
+
+    ip = start_node( log, name, get_nodespace('test_nodespace.yaml'), "")
+
+    log.info(f'Instances in project {ip}')
+    
+
+if __name__ == '__main__':    
+    do_create_instance()
+
+
