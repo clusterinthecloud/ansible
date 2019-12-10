966b9973a1f5ed669b012dd16b0e01092b8d5304
Matt Williams <matt@milliams.com>
2019-12-10 11:58:24 +0000

    Updates for CentOS

--- roles/slurm/files/citc_aws.py
+++ roles/slurm/files/citc_aws.py
@@ -70,7 +70,7 @@ def create_node_config(client, hostname: str, nodespace: Dict[str, str], ssh_key
         user_data = f.read().decode()
 
     shape = get_shape(hostname)
-    image = "ami-6e28b517"  # TODO
+    image = "ami-0ff760d16d9497662"  # TODO Owner: 679593333241 Description: CentOS Linux 7 x86_64 HVM EBS ENA 1901_01
 
     config = {
         "ImageId": image,
