f96aaa5fa1572e9ec9a3d777798c4e2c5851c5fc
Matt Williams <matt@milliams.com>
2020-03-18 19:54:08 +0000

    Make node column wider in list_nodes

--- roles/slurm/files/list_nodes.sh
+++ roles/slurm/files/list_nodes.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 SLURM_CONF=/mnt/shared/etc/slurm/slurm.conf
 
-FORMAT='nodelist,statelong:12,reason:30,cpus:5,socketcorethread:8,memory:10,features:40,gres:20,nodeaddr,timestamp'
+FORMAT='nodelist:40,statelong:12,reason:30,cpus:5,socketcorethread:8,memory:10,features:40,gres:20,nodeaddr,timestamp'
 
 sinfo --Format="${FORMAT}" | head -n1
 grep -o -E 'NodeName=([a-zA-Z0-9][a-zA-Z0-9\-]*)' "${SLURM_CONF}" | cut -c10- | while read -r node
