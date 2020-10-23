2143d87fced71edc6174a413e0be93145b90f16a
Matt Williams <matt@milliams.com>
2020-10-23 15:27:49 +0100

    Add confirmation of killing nodes

--- roles/slurm/files/kill_all_nodes.sh
+++ roles/slurm/files/kill_all_nodes.sh
@@ -3,6 +3,14 @@ set -euo pipefail
 
 # TODO Use python-citc
 
+read -p "Are you sure you want to kill all running compute nodes [y/N]? " -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo Exiting without killing nodes
+    exit 1
+fi
+echo
+
 echo Terminating any remaining compute nodes
 if systemctl status slurmctld >> /dev/null; then
     sudo -u slurm /usr/local/bin/stopnode "$(sinfo --noheader --Format=nodelist:10000 | tr -d '[:space:]')" || true
