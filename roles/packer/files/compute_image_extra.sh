7e54f6bd7a28b5db6b6dfb13fc3ac1a5cdddfcda
Matt Williams <matt@milliams.com>
2020-12-09 18:32:21 +0000

    Add comment on root for packer image fiile
    Also add some example installs

--- roles/packer/files/compute_image_extra.sh
+++ roles/packer/files/compute_image_extra.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 # Add any commands you wish to run in the compute image to this file
 # and rerun `run-packer`
+# If you need root permissions for any commands, use sudo
+
+# e.g.:
+# sudo yum -y groupinstall "Development Tools"
+# sudo yum -y install cmake gcc-gfortran
