9f8343ee13304c2ac8e54cc20232e2f850828465
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-12-14 10:35:12 +0100

    update install_cvmfs_eessi.sh script to also support aarch64 nodes

--- roles/packer/files/install_cvmfs_eessi.sh
+++ roles/packer/files/install_cvmfs_eessi.sh
@@ -7,8 +7,28 @@
 set -eu
 
 # install CernVM-FS
-sudo dnf install -y  https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
-sudo dnf install -y cvmfs
+if [[ $(arch) == "aarch64" ]]; then
+    # no package available for CernVM-FS for CentOS 8 and aarch64 systems, so building from source...
+    sudo dnf install -y cmake fuse-devel fuse3-devel fuse3-libs gcc-c++ libcap-devel libuuid-devel make openssl-devel patch python2 python3-devel unzip valgrind zlib-devel
+    curl -OL https://github.com/cvmfs/cvmfs/archive/cvmfs-2.7.5.tar.gz
+    tar xfz cvmfs-2.7.5.tar.gz
+    cd cvmfs*2.7.5
+    mkdir build
+    cd build
+    cmake ..
+    make -j $(nproc)
+    sudo make install
+
+    sudo dnf install -y attr autofs
+    sudo dnf install -y fuse
+
+    # fuse3 must be around for building, but not at runtime (for CentOS 8);
+    # causes failure to mount CernVM-FS filesystems (FUSE3 version is too old?)
+    sudo dnf remove -y fuse3-libs fuse3-devel
+else
+    sudo dnf install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
+    sudo dnf install -y cvmfs
+fi
 
 # install CernVM-FS configuration for EESSI
 sudo dnf install -y https://github.com/EESSI/filesystem-layer/releases/download/v0.2.3/cvmfs-config-eessi-0.2.3-1.noarch.rpm
