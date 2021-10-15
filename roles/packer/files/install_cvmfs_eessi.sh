95a736b92af1781b429883343ad3b12c7e3a5403
Kenneth Hoste <kenneth.hoste@ugent.be>
2021-10-15 20:09:52 +0200

    always install 'latest' version of EESSI CernVM-FS configuration

--- roles/packer/files/install_cvmfs_eessi.sh
+++ roles/packer/files/install_cvmfs_eessi.sh
@@ -30,8 +30,8 @@ else
     sudo dnf install -y cvmfs
 fi
 
-# install CernVM-FS configuration for EESSI
-sudo dnf install -y https://github.com/EESSI/filesystem-layer/releases/download/v0.2.3/cvmfs-config-eessi-0.2.3-1.noarch.rpm
+# install latest CernVM-FS configuration for EESSI
+sudo dnf install -y https://github.com/EESSI/filesystem-layer/releases/download/latest/cvmfs-config-eessi-latest.noarch.rpm
 
 # configure CernVM-FS (no proxy, 10GB quota for CernVM-FS cache)
 sudo bash -c "echo 'CVMFS_HTTP_PROXY=DIRECT' > /etc/cvmfs/default.local"
