7122c60593a68dc45a1b1b1b1465e711b0e90a3e
Matt Williams <matt@milliams.com>
2023-10-31 18:11:04 +0000

    Add Ceph network to start node script

--- roles/slurm/files/citc_openstack.py
+++ roles/slurm/files/citc_openstack.py
@@ -87,7 +87,7 @@ def create_node_config(conn, hostname: str, ip: Optional[str], nodespace: Dict[s
         "name": hostname,
         "image_id": image.id,
         "flavor_id": flavor.id,
-        "networks": [{"uuid": nodespace["network_id"]}],
+        "networks": [{"uuid": nodespace["network_id"]}, {"uuid": nodespace["ceph_network"]}],
         "user_data": user_data,
         "security_groups": [{"name": nodespace["security_group"]}],
         "block_device_mapping_v2": block_device_mapping_v2,
